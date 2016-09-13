317a263b7d9ef221991568179c397145714cc987
xiaopai <airiness021@163.com>
2016-09-13 12:21:47 +0800

    完成FixedCapacityStackOfStrings单元测试 添加测试用文件tobe.txt

--- codeOfAlgorithms/codeOfAlgorithms/mainTest.cpp
+++ codeOfAlgorithms/codeOfAlgorithms/mainTest.cpp
@@ -7,7 +7,9 @@ using namespace std;
 int main()
 {
 	FixedCapacityStackOfStrings s(100);
-	ifstream fin("tobe.txt", ios::in | ios::binary);
+	ifstream fin;
+	fin.open("tobe.txt", ios::in | ios::binary);
+
 	string item;
 	while (!fin.eof())
 	{
@@ -16,13 +18,15 @@ int main()
 		{
 			s.push(item);
 		}
-		else if (fin.eof())
+		else if (!fin.eof())
 		{
 			cout << s.pop() << " ";
 		}
 	}
 	cout << endl;
 	cout << s.size() << endl;
+	fin.close();
+	system("pause");
 	return 0;
 
 }
