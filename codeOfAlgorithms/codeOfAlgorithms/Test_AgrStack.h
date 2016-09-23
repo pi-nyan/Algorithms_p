03be039ed8ee493172bd33b26e517f4f6d702ab6
xiaopai <airiness021@163.com>
2016-09-23 14:00:01 +0800

    添加stack测试

--- codeOfAlgorithms/codeOfAlgorithms/Test_AgrStack.h
+++ codeOfAlgorithms/codeOfAlgorithms/Test_AgrStack.h
@@ -1,1 +1,17 @@
 #pragma once
+#ifndef TEST_AGR_STACK_H
+#define TEST_AGR_STACK_H
+
+#include "AgrStack.h"
+
+#include <string>
+#include <fstream>
+#include <iostream>
+using namespace std;
+
+namespace AlgorithmsTest
+{ 
+	void tAgrStack();
+}
+
+#endif
--- codeOfAlgorithms/codeOfAlgorithms/mainTest.cpp
+++ codeOfAlgorithms/codeOfAlgorithms/mainTest.cpp
@@ -1,12 +1,14 @@
 #include "Test_ResizingArrayStack.h"
 #include "Test_FixedCapacityStack.h"
 #include "Test_FixedCapacityStackOfStrings.h"
+#include "Test_AgrStack.h"
 
 int main()
 {
 	//AlgorithmsTest::tResizingArrayStack();
 	//AlgorithmsTest::tFixedCapacityStack();
 	//AlgorithmsTest::tFixedCapacityStackOfStack();
+	AlgorithmsTest::tAgrStack();
 	system("pause");
 	return 0;
 
